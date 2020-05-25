3e06c4a5e52af48a27539c20bb12d6f20db60756
LynnAmsbury <Lynn8542@gmail.com>
2020-05-25 15:40:38 -0600

    Done.

--- lib/student.rb
+++ lib/student.rb
@@ -3,13 +3,12 @@ require_relative "../config/environment.rb"
 class Student
 
   # Remember, you can access your database connection anywhere in this class
-  #  with DB[:conn]  
+  #  with DB[:conn]
   
   attr_accessor :name, :grade
-  
   attr_reader :id
   
-  def initialize(name, grade, id=nil)
+  def initialize(name, grade, id = nil)
     @id = id
     @name = name
     @grade = grade
@@ -23,12 +22,12 @@ class Student
         grade TEXT
         )
     SQL
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
 
   def self.drop_table
     sql = "DROP TABLE IF EXISTS students"
-    DB[:conn].execute(sql) 
+    DB[:conn].execute(sql)
   end
   
   def save
